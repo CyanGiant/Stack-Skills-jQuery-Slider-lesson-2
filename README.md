8d9a0ed0a801358e2cc38a3436a2c53e2bd6a8b4
James Bennett <J.bennett@gmx.com>
2015-08-04 16:26:24 -0700

    initial commit for intro part of lesson. teaches event handlers for mouseovers, clicks, able to append, change class etc... http://stackskills.com/courses/projects-in-javascript-jquery/lectures/136560

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Stack-Skills-jQuery-Slider-lesson-2
+This Slider in jQuery is Lesson 2 from Stackskills.com.
+
---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+//   $('.mylink').on('mouseenter', function(){
+//     $('h1').addClass('red');
+//   });
+//   $('.mylink').on('mouseleave', function(){
+//     $('h1').removeClass('red');
+// });
+
+  $('.mylink').click(function(){
+    //$('h1').append('<h3>Hello Earth</h3>');
+    $('h1').css('font-size', '40px');
+  });
+
+});
